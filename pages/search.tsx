f46d852723561d1c9cdeefccf59ebfc111a6f16a
Elia <mkumboelia@gmail.com>
2023-04-19 18:44:09 +0300

    limited access of typing to authenticated users

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { useRouter } from "next/router";
+import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
+
+// Local imports
+import { useAuth } from "@/utils/use-auth";
+import { auth, google_provider } from "@/firebase";
 
 type ResultDataType = {
     id: number,
@@ -35,8 +42,11 @@ const sampleData: Array<ResultDataType> = [
 ]
 
 export default function Search() {
-    const [query, setQuery] = useState("");
     const [results, setResults] = useState<Array<ResultDataType>>(sampleData);
+    const [loading, setloading] = useState(false)
+    const [query, setQuery] = useState("");
+    const navigate = useRouter()
+    const auth_ = useAuth()
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
@@ -47,6 +57,20 @@ export default function Search() {
             // setResults(results);
         } catch (error) {
             console.error(error);
+        }
+    };
+
+    const withGoogle = async () => {
+        try {
+            setloading(true);
+            google_provider.setCustomParameters({ prompt: "select_account" });
+            await signInWithPopup(auth, google_provider);
+            //   navigate.push("/");
+        } catch (error) {
+            setloading(false);
+            const error_message = (error as Error).message;
+            console.log(error_message)
+            //   seterror(error_message);
         }
     };
 
@@ -86,22 +110,24 @@ export default function Search() {
                     </div>
                 </div>
             </nav>
-            <div className="px-4 sm:px-6 lg:px-8 py-20">
-                <h1 className="text-3xl text-center font-bold mb-8">Describe a small story, parable or event in the Bible.</h1>
-                <form onSubmit={handleSubmit} className="max-w-5xl mx-auto">
-                    <textarea
-                        name="query"
-                        rows={5}
-                        value={query}
-                        onChange={(event) => setQuery(event.target.value)}
-                        placeholder="Type here..."
-                        className="py-2 px-3 rounded-md w-full text-gray-800 border-gray-300 outline-none"
-                    />
-                    <button type="submit" className="block border w-fit mx-auto mt-10 px-9 py-2 rounded">
-                        Search
-                    </button>
-                </form>
-                {/* {results.length > 0 && (
+            {
+                auth_.user ?
+                    <div className="px-4 sm:px-6 lg:px-8 py-20">
+                        <h1 className="text-3xl text-center font-bold mb-8">Describe a small story, parable or event in the Bible.</h1>
+                        <form onSubmit={handleSubmit} className="max-w-5xl mx-auto">
+                            <textarea
+                                name="query"
+                                rows={5}
+                                value={query}
+                                onChange={(event) => setQuery(event.target.value)}
+                                placeholder="Type here..."
+                                className="py-2 px-3 rounded-md w-full text-gray-800 border-gray-300 outline-none"
+                            />
+                            <button type="submit" className="block border w-fit mx-auto mt-10 px-9 py-2 rounded">
+                                Search
+                            </button>
+                        </form>
+                        {/* {results.length > 0 && (
                     <div className="mt-2">
                         <p className="text-lg font-medium mb-4">Search results:</p>
                         <ul className="list-disc pl-8">
@@ -115,7 +141,30 @@ export default function Search() {
                         </ul>
                     </div>
                 )} */}
-            </div>
+                    </div> :
+                    <div className="py-20 space-y-10">
+                        <h3 className="mx-auto text-center max-w-4xl text-xl font-bold  text-gray-100 sm:text-3xl mb-5">
+                            Have a story you remember and want to know what <span className="text-gray-600">Bible Scripture</span> it was?
+                        </h3>
+                        <div className="h-[250px] flex flex-col items-center space-y-10 max-w-[670px] mt-2 mx-auto">
+                            <div className="max-w-xl text-gray-300 text-lg">
+                                Sign up to get free 3 stories to ask.
+                            </div>
+                            <button
+                                onClick={() => withGoogle()}
+                                className="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2"
+                            >
+                                <Image
+                                    src="/google.png"
+                                    width={20}
+                                    height={20}
+                                    alt="google's logo"
+                                />
+                                <span>Sign in with Google</span>
+                            </button>
+                        </div>
+                    </div>
+            }
         </div>
     );
 }
