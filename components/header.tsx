8cae38bc9473e278c73d3eb21d8b642353623227
Elia <mkumboelia@gmail.com>
2023-05-05 16:14:43 +0300

    :wrench: homepage and header responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -45,11 +45,20 @@ function Header() {
                         }
                     </div>
                     <div className="flex -mr-2 sm:hidden">
-                        <button
-                            type="button"
-                            className="inline-flex items-center justify-center p-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500"
-                            aria-label="Main menu"
-                            aria-expanded="false"
+                        <Popover content={
+                            <div className="px-4 py-2">
+                                <div className='flex items-center justify-center pb-1 space-x-2 border-b border-gray-400'>
+                                    <Avatar user_image={auth_.user?.photoURL} />
+                                    <p className="font-medium text-gray-700">{auth_.user?.displayName}</p>
+                                </div>
+                                <div className='flex items-center justify-center pt-1 space-x-2 text-red-700 border cursor-pointer' onClick={() => signOut(auth)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                        <path fillRule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                                    </svg>
+                                    <p> Sign out </p>
+                                </div>
+                            </div>}
                         >
                             <svg className="block w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
@@ -57,7 +66,7 @@ function Header() {
                             <svg className="hidden w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                             </svg>
-                        </button>
+                        </Popover>
                     </div>
                 </div>
             </div >
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,10 @@ export default function Home() {
 
       <div className="px-4 py-16 mx-auto my-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="text-center">
-          <h1 className="text-4xl font-bold leading-9 sm:text-5xl sm:leading-10">
+          <h1 className="text-3xl font-bold leading-9 md:text-4xl sm:text-5xl sm:leading-10">
             Discover the Bible in a whole new way
           </h1>
-          <p className="max-w-2xl mx-auto mt-3 text-xl leading-7 sm:mt-4">
+          <p className="max-w-2xl mx-auto mt-3 text-lg leading-7 md:text-xl sm:mt-4">
             Search for specific Bible verses by describing a small story, parable or event in the Bible.
           </p>
           <div className="max-w-md mx-auto mt-10">
