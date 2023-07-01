e43d079e14c4f491199306ee6d7849e0a4425eab
Elia <mkumboelia@gmail.com>
2023-07-01 12:22:07 +0300

    :warning:  feat: alerting when credits are running low

--- pages/search.tsx
+++ pages/search.tsx
@@ -14,6 +14,7 @@ import { auth, db, google_provider } from "@/firebase";
 import Ellipsis from "@/components/ellipsis/ellipsis";
 import { Data } from "./api/search";
 import Footer from "@/components/footer";
+import Popover from "@/components/popover";
 
 type UserDataType = {
     credits: number | null
@@ -43,6 +44,10 @@ export default function Search() {
                     toast.error("Failed to fetch your credits.")
                     console.error(`${(error as Error).message}`)
                 })
+        }
+
+        if (userData?.credits !== null && userData?.credits >= 1 && userData?.credits <= 3) {
+            toast("You are running low on credits", { icon: "⚠️" })
         }
 
     }, [auth_.user, userData])
@@ -155,9 +160,26 @@ export default function Search() {
                                 value={query}
                                 onChange={(event) => setQuery(event.target.value)}
                                 placeholder="Type here..."
-                                className={`w-full p-2.5 text-sm md:text-base text-gray-800 border-gray-300 rounded-md outline-none ${userData.credits === 0 ? "ring-4 ring-red-700" : "ring-4 ring-green-700"}`}
+                                className={`w-full p-2.5 text-sm md:text-base text-gray-800 border-gray-300 rounded-md outline-none ${userData.credits ?
+                                    userData.credits <= 3 ? "ring-4 ring-yellow-500" :
+                                        userData.credits === 0 ? "ring-4 ring-red-700" : "ring-4 ring-green-700" :
+                                    "ring-4 ring-gray-800"
+                                    }`}
                             />
-                            <p className="text-sm text-gray-500 md:text-base text-end">Quota remaining: {userData?.credits}</p>
+                            <div className="flex items-center justify-end mt-2">
+                                <Popover content={
+                                    <div className="flex flex-col items-center justify-center p-4 space-y-2">
+                                        <h3 className="font-bold text-gray-800">How to pay</h3>
+                                        <p className="text-sm text-center text-gray-800">We currently don't support online payment, you can contact me through <Link className="text-blue-600 underline" href="mailto:mkumboelia@gmail.com">my email</Link></p>
+                                    </div>
+                                }>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-info-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                                    </svg>
+                                </Popover>
+                                <p className="ml-3 text-sm text-gray-500 md:text-base">Quota remaining: {userData?.credits}</p>
+                            </div>
                             <button type="submit" disabled={loading} className={`${loading && "opacity-30"} block px-7 py-2 md:px-9 mx-auto mt-10 border rounded w-fit`}>
                                 Search
                             </button>
