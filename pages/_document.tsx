8667f2cbb6f60c9a0571fa385b0d12dbee004dae
Jackson Makinda <me@makindajackson.com>
2023-07-05 00:10:28 +0300

    :a: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  let description = "Discover bible references for your favorite stories, events, and parables with find-bible-scripture.netlify.app. Easy to use";
+  let description =
+    "Discover bible references for your favorite stories, events, and parables with find-bible-scripture.netlify.app. Easy to use";
   let ogimage = "https://find-bible-scripture.netlify.app/bible_with_cup.jpg";
   let sitename = "Find Bible Scripture";
   let title = "Find Bible Scripture | Discover the Bible in a whole new way";
@@ -14,14 +15,20 @@ export default function Document() {
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={sitename} />
-        <meta property="og:url" content="https://find-bible-scripture.netlify.app/" />
+        <meta
+          property="og:url"
+          content="https://find-bible-scripture.netlify.app/"
+        />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={ogimage} />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://find-bible-scripture.netlify.app/" />
+        <meta
+          property="twitter:url"
+          content="https://find-bible-scripture.netlify.app/"
+        />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={ogimage} />
@@ -31,5 +38,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
