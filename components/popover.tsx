c7937e8105d54a1f6687c3914ed7489904350ebe
Jackson Makinda <me@makindajackson.com>
2023-07-10 08:39:03 +0300

    Update popover.tsx

--- components/popover.tsx
+++ components/popover.tsx
@@ -1,39 +1,42 @@
-import { useState, ReactNode, useRef } from 'react'
-import useOnClickOutside from './hooks/useClickOutside';
+import { useState, ReactNode, useRef } from "react";
+import useOnClickOutside from "./hooks/useClickOutside";
 
 type PopoverProps = {
-    content: ReactNode;
-    children?: ReactNode;
+  content: ReactNode;
+  children?: ReactNode;
 };
 
 function Popover({ content, children }: PopoverProps) {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const ref = useRef(null)
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const ref = useRef(null);
 
-    useOnClickOutside(ref, () => setIsOpen(false))
+  useOnClickOutside(ref, () => setIsOpen(false));
 
-    const togglePopover = () => {
-        setIsOpen(!isOpen);
-    };
+  const togglePopover = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className="relative">
-            <div
-                className="inline-block cursor-pointer"
-                onClick={togglePopover}
-            >
-                {children}
-            </div>
+  return (
+    <div className="relative">
+      <div
+        className="inline-block cursor-pointer text-azure-950 dark:text-azure-50"
+        onClick={togglePopover}
+      >
+        {children}
+      </div>
 
-            {isOpen && (
-                <div ref={ref} className="absolute right-0 z-10 w-48 mt-1 bg-white rounded-md shadow-lg top-full ring-1 ring-black ring-opacity-5">
-                    <div className="py-1" aria-orientation="vertical">
-                        {content}
-                    </div>
-                </div>
-            )}
+      {isOpen && (
+        <div
+          ref={ref}
+          className="absolute right-0 z-10 w-48 mt-1 bg-white rounded-md shadow-lg top-full ring-1 ring-black ring-opacity-5"
+        >
+          <div className="py-1" aria-orientation="vertical">
+            {content}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
-export default Popover
+export default Popover;
