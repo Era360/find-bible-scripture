60d561389c05342749a8ace30fee8d8859ca7f83
Elia <mkumboelia@gmail.com>
2026-04-15 14:21:07 +0300

    Use remotePatterns for Next image and bump deps
    Replace images.domains with Next.js images.remotePatterns for lh3.googleusercontent.com and tidy next.config.js formatting. Update package.json and tsconfig.json (dependency/version bumps applied) and regenerate package-lock.json.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
